27ce2c095ac12e482b4bacf3b5ebcc3a7a85f732
Meaghan <meaghan.sharrard89@gmail.com>
2023-12-11 17:29:53 -0800

    passing tests

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,30 @@
-def roll_call_dwarves():
+def roll_call_dwarves(list):
+    for index, name in enumerate(list):
+        print(f"{index + 1}. {name}")
     pass
 
-def summon_captain_planet():
+
+def summon_captain_planet(calls):
+    list = []
+    for call in calls:
+        list.append(call.capitalize() + "!")
+    return list
     pass
 
-def long_planeteer_calls():
+
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
     pass
 
-def find_the_cheese():
+
+def find_the_cheese(list):
+    cheeses = ("cheddar", "gouda", "camembert")
+    for item in list:
+        if item in cheeses:
+            return item
+    else:
+        return None
     pass
